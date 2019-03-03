6e0da31ce6e2272fe1e6525ee57c9e5d66c96a30
Sushil <sushil.s@greyorange.sg>
2019-03-03 23:44:48 +0530

    Update ApplicationDbContext.cs
    Completed the module

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -5,7 +5,6 @@ namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        //Test
         public DbSet<CelestialObject> CelestialObjects { get; set; }
 
         public ApplicationDbContext(DbContextOptions options) : base(options)
